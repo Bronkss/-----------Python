8146e259452c2a217851905fd4ffc1f60f2cdec2
Alex <alexandr.yakimov.heaven@gmail.com>
2023-07-20 16:39:04 +0300

    Импортировали библиотеки, сделали окно функционала

---
+++ notes.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfile, asksaveasfile
+
+root = Tk()
+root.title("Заметки")
+root.geometry("400x500")
+
+root.mainloop()
