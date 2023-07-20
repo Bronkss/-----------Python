15466c753c7a206df6487c993ef67bc0860c7509
Alex <alexandr.yakimov.heaven@gmail.com>
2023-07-20 17:40:28 +0300

    Добавили функцию нового файла и возможность писать текстом в поле заметки

--- notes.py
+++ notes.py
@@ -2,8 +2,23 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfile, asksaveasfile
 
+file_name = NONE
+
+def new_file():
+    global file_name
+    file_name = "Без названия"
+    text.delete('1.0', END)
+
 root = Tk()
 root.title("Заметки")
-root.geometry("400x500")
+root.geometry("400x400")
 
+text = Text(root, width=400, height=400)
+text.pack()
+
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+
+root.config(menu=menu_bar)
 root.mainloop()
